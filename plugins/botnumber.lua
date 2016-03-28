3271409d971e1312e6e5ac664043aec40ef84395
parsa13881 <mohammadpa87@gmail.com>
2016-03-28 10:37:06 +0430

    Update botnumber.lua

--- plugins/botnumber.lua
+++ plugins/botnumber.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+13603353378", "NOD32", "BOT", ok_cb, false)
+send_contact(get_receiver(msg), "+18484565352", "Fire", "BOT", ok_cb, false)
 end
 
 return {
