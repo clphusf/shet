e443efe02cb48bb6b0544b119e09cc8701323416
parsa13881 <mohammadpa87@gmail.com>
2016-04-11 04:27:15 -0700

    Create share.lua

---
+++ plugins/share.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+
+if matches[1] == 'share' then
+send_contact(get_receiver(msg), "+13863138911", "Fire Bot", "Use /Share To Get Number", ok_cb, false)
+end
+end
+return {
+patterns = {
+"^[#!/]([Ss][Hh][Aa][Rr][Ee])$",
+"^([Ss][Hh][Aa][Rr][Ee])$"
+
+},
+run = run
+}
+
+end
